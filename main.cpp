2ead5e81277df5115bbc8a62e57bc90ffed1b2c0
Patrick Smith <89660970+thisguyblink@users.noreply.github.com>
2023-10-21 13:38:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
-nothing
+#include <iostream>
+#include "MyVector.hpp"
+
+///	Your welcome
+using std::cout;
+using std::endl;
+
+//
+int main()
+{
+	
+	MyVector<int> test1;
+	MyVector<std::string> test2;
+	for (int i = 0; i < 5; i++) {
+		test1.push_back(i);
+	}
+	test1.reserve(100);
+	std::cout<< test1.capacity() << endl;
+	std::cout<< test1.size() << endl;
+	for (size_t i = 0; i < test1.size(); i++) {
+		std::cout << test1.at(i);
+	}
+	
+	return 0;
+}
